13c557a64988c4a0379b62a7dc2a637c3b38d672
D00191063 <D00191063@P1106-PC18.ad.dkit.ie>
2017-01-27 12:20:05 +0000

    My method

---
+++ FridayGitDkIT/src/fridaygitdkit/FridayGitDkIT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fridaygitdkit;
+
+/**
+ *
+ * @author D00191063
+ */
+public class FridayGitDkIT {
+
+    void seansSuperiorMethod(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
